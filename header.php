e10196bd44162665f20913897765886892230476
Geoffroy <geoffroy.c@codeur.online>
2018-10-05 22:33:48 +0200

    Initialisation du projet

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+        <img class="img_header" src="media/avatar.png" height="256" width="256" alt="avatar homme casquette noir, lunette, pull gris">
+        <img class="huitre_logo" src="media/huitre.png" height="217" width="217" alt="logo d'une huitre">
+        <h1>GEOFFROY SANCHEZ</h1>
+        <div class="sub_h1">OSTRÉICULTURE DE PÉRE EN FILS</div>
+</header>
+    <nav class="navbar">
+        <a class="white_link" href="coucou.html" target="_blank">ACHETER</a>
+        <a class="white_link" href="coucou.html" target="_blank">L'HISTOIRE</a>
+        <a class="white_link" href="coucou.html" target="_blank">CALENDRIER</a>
+        <a class="white_link" href="contact.html" target="_blank">CONTACT</a>
+        <a class="white_link" href="coucou.html" target="_blank">À PROPOS</a>
+    </nav>
